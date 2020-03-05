dc468fc3cca7e4eb2fdb8c0673152e843163bff3
Sitong Liu <liusit@foxmail.com>
2020-03-04 22:01:35 -0800

    Create title_search.py

---
+++ util/title_search.py
@@ -0,0 +1,15 @@
+def get_ids_called(title):
+    ids = []
+    f = open('data/mxm_779k_matches.txt', "r", encoding='utf-8')
+    for line in f.readlines():
+        line = line.rstrip('\n')
+        if line[0] == '#':
+            continue
+        tid1, artist1, title1, tid2, artist2, title2 = line.split('<SEP>')
+        if title2.lower() == title.lower():
+            ids.append(tid2)
+    return ids
+
+print(get_ids_called('Dream'))
+print(get_ids_called('dangerous'))
+print(get_ids_called('why'))
